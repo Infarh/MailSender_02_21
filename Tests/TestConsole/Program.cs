f3d0e9c87e4ec27d0010b7564a942e0fd62423d2
Infarh <shmachilin@yandex.ru>
2021-02-12 19:49:42 +0300

    Логика отправки почты SMTP

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace TestConsole
 {
@@ -6,7 +8,24 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var from = new MailAddress("shmachilin@yandex.ru", "Павел");
+            var to = new MailAddress("shmachilin@gmail.com", "Павел");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "Заголовок";
+            message.Body = "Текст письма";
+
+            var client = new SmtpClient("smtp.yandex.ru", 465);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "UserName",
+                //SecurePassword = 
+                Password = "PAssword!"
+            };
+
+            client.Send(message);
         }
     }
 }
