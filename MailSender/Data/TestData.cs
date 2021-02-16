389475c5ceadc9539df2dbcc758d5ccd298f3699
Infarh <shmachilin@yandex.ru>
2021-02-16 21:01:45 +0300

    Добавление тестовых данных в разметку

---
+++ MailSender/Data/TestData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MailSender.Models;
+
+namespace MailSender.Data
+{
+    internal static class TestData
+    {
+        public static List<Server> Servers { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Server
+            {
+                Name = $"Сервер-{i}",
+                Address = $"smtp.server{i}.com",
+                Login = $"Login-{i}",
+                Password = $"Password-{i}",
+                UseSSL = i % 2 == 0,
+            })
+           .ToList();
+
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Sender
+            {
+                Name = $"Отправитель {i}",
+                Address = $"sender_{i}@server.ru"
+           })
+           .ToList();
+
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Recipient
+            {
+                Name = $"Получатель {i}",
+                Address = $"recipient_{i}@server.ru"
+            })
+           .ToList();
+
+        public static List<Message> Messages { get; } = Enumerable.Range(1, 100)
+           .Select(i => new Message
+            {
+                Title = $"Сообщение {i}",
+                Text = $"Текст сообщения {i}"
+            })
+           .ToList();
+    }
+}
--- MailSender/Models/Server.cs
+++ MailSender/Models/Server.cs
@@ -4,11 +4,13 @@
     {
         public string Name { get; set; }
         public string Address { get; set; }
-        public int Port { get; set; }
+        public int Port { get; set; } = 25;
 
         public bool UseSSL { get; set; }
 
         public string Login { get; set; }
         public string Password { get; set; }
+
+        public override string ToString() => $"{Name}:{Port}";
     }
 }
