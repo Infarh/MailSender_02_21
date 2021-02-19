f6bfdb90ede9a386c1c9732c085885ed526493b3
Infarh <shmachilin@yandex.ru>
2021-02-19 20:18:45 +0300

    Модель-представления Главного окна основного проекта

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System.Windows;
+using MailSender.ViewModels;
 
 namespace MailSender
 {
     public partial class MainWindow
     {
-        public MainWindow() => InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DataContext = new MainWindowViewModel();
+        }
 
         private void Exit_Click(object sender, RoutedEventArgs e) => Close();
     }
---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.lib.ViewModels.Base;
+
+namespace MailSender.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Рассыльщик";
+
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        private string _Status = "Готов!";
+
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+    }
+}
