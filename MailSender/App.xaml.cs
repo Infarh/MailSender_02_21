e8c23e241e70b8832ca1db0d2f634f260a5908d0
Infarh <shmachilin@yandex.ru>
2021-02-19 20:34:03 +0300

    Контейнер сервисов

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,17 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+using MailSender.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MailSender
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => 
+            Host.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration(opt => opt.AddJsonFile("appsettings.json", false, true))
+               .ConfigureServices(ConfigureServices)
+            ;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
---
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MailSender.ViewModels
+{
+    internal class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
