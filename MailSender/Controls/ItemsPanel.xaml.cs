20de5bc7bfd99aadc36c5443391b21432750f8c5
Infarh <shmachilin@yandex.ru>
2021-02-19 19:43:42 +0300

    Свойство-зависимости

--- MailSender/Controls/ItemsPanel.xaml.cs
+++ MailSender/Controls/ItemsPanel.xaml.cs
@@ -1,10 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MailSender.Controls
 {
     public partial class ItemsPanel : UserControl
     {
-        public string Title { get => Header.Text; set => Header.Text = value; }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(string)));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
 
         public ItemsPanel() => InitializeComponent();
     }
