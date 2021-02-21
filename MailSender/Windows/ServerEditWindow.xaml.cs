78a12c931f8e465ece7b9f20ac018eb38fc038a1
kanadeiar <kanadeiar@gmail.com>
2021-02-21 15:04:13 +0400

    Логика окна редактирования сервера

--- MailSender/Windows/ServerEditWindow.xaml.cs
+++ MailSender/Windows/ServerEditWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -12,6 +13,58 @@ namespace MailSender.Windows
         public ServerEditWindow()
         {
             InitializeComponent();
+        }
+        /// <summary> Диалог в режиме редактирования сервера</summary>
+        /// <param name="Title">Заголовок</param>
+        /// <param name="Name">Имя сервера</param>
+        /// <param name="Address">Адрес сервера</param>
+        /// <param name="Port">Порт сервера</param>
+        /// <param name="UseSsl">Шифрование</param>
+        /// <param name="Login">Логин</param>
+        /// <param name="Password">Пароль</param>
+        /// <returns>Успешное выполнение</returns>
+        public static bool ShowDialog(string Title, ref string Name, ref string Address, ref int Port,
+            ref bool UseSsl, ref string Login, ref string Password)
+        {
+            var window = new ServerEditWindow
+            {
+                Title = Title,
+                TextBoxServerName = { Text = Name },
+                TextBoxServerAddress = { Text = Address },
+                TextBoxServerPort = { Text = Port.ToString() },
+                CheckBoxServerSsl = { IsChecked = UseSsl },
+                TextBoxLogin = { Text = Login },
+                PasswordBoxPassword = { Password = Password },
+                Owner = Application.Current.Windows.Cast<Window>().FirstOrDefault(win => win.IsActive),
+            };
+            if (window.ShowDialog() != true) return false;
+            Name = window.TextBoxServerName.Text;
+            Address = window.TextBoxServerAddress.Text;
+            Port = int.Parse(window.TextBoxServerPort.Text);
+            UseSsl = window.CheckBoxServerSsl.IsChecked == true;
+            Login = window.TextBoxLogin.Text;
+            Password = window.PasswordBoxPassword.Password;
+            return true;
+        }
+        /// <summary> Диалог в режиме создания сервера </summary>
+        /// <param name="Name">Имя</param>
+        /// <param name="Address">Адрес</param>
+        /// <param name="Port">Порт</param>
+        /// <param name="UseSsl">Использование шифрования</param>
+        /// <param name="Login">Логин</param>
+        /// <param name="Password">Пароль</param>
+        /// <returns>Успешное выполнение</returns>
+        public static bool Create(out string Name, out string Address, out int Port, out bool UseSsl,
+            out string Login, out string Password)
+        {
+            Name = default;
+            Address = default;
+            Port = 25;
+            UseSsl = default;
+            Login = default;
+            Password = default;
+            return ShowDialog("Создать почтовый сервер", ref Name, ref Address, ref Port, ref UseSsl,
+                ref Login, ref Password);
         }
 
 
