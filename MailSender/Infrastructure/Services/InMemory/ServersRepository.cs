ce8b4e925640df58d9b1ce35a038d38f5cd0c1eb
Infarh <shmachilin@yandex.ru>
2021-02-24 19:57:08 +0300

    Репозиторий серверов через базовый класс

--- MailSender/Infrastructure/Services/InMemory/ServersRepository.cs
+++ MailSender/Infrastructure/Services/InMemory/ServersRepository.cs
@@ -6,46 +6,25 @@ using MailSender.Models;
 
 namespace MailSender.Infrastructure.Services.InMemory
 {
-    class ServersRepository : IRepository<Server>
+    class ServersRepository : RepositoryInMemory<Server>
     {
-        private List<Server> _Servers;
-        private int _MaxId;
-
-        public ServersRepository()
+        public ServersRepository() : base(Enumerable.Range(1, 10)
+           .Select(i => new Server
+            {
+                Id = i,
+                Name = $"Сервер-{i}",
+                Address = $"smtp.server{i}.com",
+                Login = $"Login-{i}",
+                Password = TextEncoder.Encode($"Password-{i}", 7),
+                UseSSL = i % 2 == 0,
+            }))
         {
-            _Servers = Enumerable.Range(1, 10)
-               .Select(i => new Server
-               {
-                   Id = i,
-                   Name = $"Сервер-{i}",
-                   Address = $"smtp.server{i}.com",
-                   Login = $"Login-{i}",
-                   Password = TextEncoder.Encode($"Password-{i}", 7),
-                   UseSSL = i % 2 == 0,
-               })
-               .ToList();
-            _MaxId = _Servers.Max(s => s.Id);
         }
 
-        public IEnumerable<Server> GetAll() => _Servers;
-        public Server GetById(int id) => _Servers.FirstOrDefault(s => s.Id == id);
-
-        public int Add(Server item)
+        public override void Update(Server item)
         {
-            if (_Servers.Contains(item))
-                return item.Id;
-            item.Id = ++_MaxId;
-            _Servers.Add(item);
-            return item.Id;
-        }
-
-        public void Update(Server item)
-        {
-            if(_Servers.Contains(item))
-                return;
-
             var db_item = GetById(item.Id);
-            if (db_item is null) return;
+            if (db_item is null || ReferenceEquals(db_item, item)) return;
 
             db_item.Name = item.Name;
             db_item.Address = item.Address;
@@ -53,7 +32,5 @@ namespace MailSender.Infrastructure.Services.InMemory
             db_item.Login = item.Login;
             db_item.Password = item.Password;
         }
-
-        public bool Remove(int id) => _Servers.RemoveAll(s => s.Id == id) > 0;
     }
 }
