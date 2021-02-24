88a75088cbaa3020b08eafb7b30646d56f6ec5fd
Infarh <shmachilin@yandex.ru>
2021-02-24 20:50:42 +0300

    Валидация даных с помощью интерфейса

--- MailSender/Models/Recipient.cs
+++ MailSender/Models/Recipient.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
 using MailSender.Models.Base;
 
 namespace MailSender.Models
 {
-    public class Recipient : Entity
+    public class Recipient : Entity, IDataErrorInfo
     {
         private string _Name;
         public string Name
@@ -19,5 +20,28 @@ namespace MailSender.Models
         }
 
         public string Address { get; set; }
+
+
+        string IDataErrorInfo.Error => null;
+
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
+            {
+                switch (PropertyName)
+                {
+                    default: return null;
+
+                    case nameof(Name):
+                        var name = Name;
+
+                        if (name is null) return "Имя не может быть пустой ссылкой";
+                        if (name.Length < 3) return "Длина имени должна быть не менее 3 символов";
+                        if (name.Length > 20) return "Длина имени должна быть не более 20 символов";
+
+                        return null;
+                }
+            }
+        }
     }
 }
