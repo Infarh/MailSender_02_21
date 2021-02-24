38c4b0edbcf35b807540f871d3a037b29ccfe880
Infarh <shmachilin@yandex.ru>
2021-02-24 19:13:10 +0300

    Отладочные конвертеры

--- MailSender/ViewModels/ViewModelLocator.cs
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -4,7 +4,14 @@ namespace MailSender.ViewModels
 {
     internal class ViewModelLocator
     {
-        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+        public MainWindowViewModel MainWindowModel
+        {
+            get
+            {
+                var model = App.Services.GetRequiredService<MainWindowViewModel>();
+                return model;
+            }
+        }
 
         public StatisticViewModel Statistic => App.Services.GetRequiredService<StatisticViewModel>();
     }
