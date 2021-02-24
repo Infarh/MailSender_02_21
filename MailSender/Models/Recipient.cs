0f2c6d8ea868eef45259e83e32d14d590db73565
Infarh <shmachilin@yandex.ru>
2021-02-24 20:44:17 +0300

    Валидация по средствам генерации исключений в сеттерах свойств

--- MailSender/Models/Recipient.cs
+++ MailSender/Models/Recipient.cs
@@ -1,10 +1,23 @@
-﻿using MailSender.Models.Base;
+﻿using System;
+using MailSender.Models.Base;
 
 namespace MailSender.Models
 {
     public class Recipient : Entity
     {
-        public string Name { get; set; }
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Не задано имя!");
+
+                _Name = value;
+            }
+        }
+
         public string Address { get; set; }
     }
 }
