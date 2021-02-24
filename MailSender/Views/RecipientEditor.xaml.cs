d0f9318e9aa66e57d322f82b10245c49c272619b
Infarh <shmachilin@yandex.ru>
2021-02-24 20:40:39 +0300

    Отображение всплывающей подсказки с результатами валидации

--- MailSender/Views/RecipientEditor.xaml.cs
+++ MailSender/Views/RecipientEditor.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MailSender.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для RecipientEditor.xaml
-    /// </summary>
-    public partial class RecipientEditor : UserControl
+    public partial class RecipientEditor
     {
         public RecipientEditor()
         {
             InitializeComponent();
+        }
+
+        private void OnNameValidationError(object Sender, ValidationErrorEventArgs E)
+        {
+            if (E.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control) Sender).ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)Sender).ClearValue(ToolTipProperty);
+            }
+        }
+
+        private void OnIdValidationError(object Sender, ValidationErrorEventArgs E)
+        {
+            if (E.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control)Sender).ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)Sender).ClearValue(ToolTipProperty);
+            }
         }
     }
 }
